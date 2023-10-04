7c891b73bc0d6447d4fae82e6f37002c60fb3514
AlinaTantsura <alinatanst@gmail.com>
2023-10-04 13:00:14 +0300

    Add Vimeo

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,11 @@
+import Vimeo from "@vimeo/player";
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo(iframe);
+let time = 0;
+
+player.on('timeupdate', onPlay);
+function onPlay(event) {
+    time = event.seconds;
+    console.log(time);
+}
