ab2b7f8a95f2cc4efcfcff5a9f1991a0cebd1efb
AlinaTantsura <133903876+AlinaTantsura@users.noreply.github.com>
2023-10-07 10:56:47 +0300

    Update the third task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,18 +3,19 @@ import throttle from "lodash.throttle";
 const form = document.querySelector('.feedback-form');
 const localS_key = "feedback-form-state";
 const obj = {};
-const infoJson = localStorage.getItem(localS_key);
-
-const infoObj = JSON.parse(infoJson) ?? {};
+const infoObj = JSON.parse(localStorage.getItem(localS_key)) ?? {};
 
 form.elements.email.value = infoObj.email ?? "";
 form.elements.message.value = infoObj.message ?? "";
 form.addEventListener('input', handleInput);
+form.addEventListener('input', throttle(addApplication, 500));
 form.addEventListener('submit', handleSubmit);
 
 function handleInput(event) {
     obj.email = event.currentTarget.email.value;
     obj.message = event.currentTarget.message.value;
+}
+function addApplication(){
     const infoToLocalStorage = JSON.stringify(obj);
     localStorage.setItem(localS_key, infoToLocalStorage);
 }
@@ -23,7 +24,7 @@ function handleSubmit(event) {
     if (!event.currentTarget.email.value || !event.currentTarget.message.value) {
         return;
     }
-    console.log(infoObj);
+    console.log(JSON.parse(localStorage.getItem(localS_key)));
     localStorage.removeItem(localS_key);
     form.reset();
 }
